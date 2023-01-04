1dc42b22f338e1eefaa835d8d736fc7168d71070
Marcus <89999325+EncryRose@users.noreply.github.com>
2023-01-04 07:57:58 +0300

    Add files via upload

---
+++ api/database.sql
@@ -0,0 +1,10 @@
+create table ac.cheater
+(
+    id        int          null,
+    profile   varchar(255) null,
+    steamid64 bigint       null,
+    date      datetime     null,
+    reason    varchar(255) null
+);
+
+
